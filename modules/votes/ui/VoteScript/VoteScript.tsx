7c3c8a5c7296151f8b8c4e347e0c2aca1a72511f
Azat Serikov <azatsdev@gmail.com>
2022-04-28 16:05:17 +0600

    fix: fix binary script display

--- modules/votes/ui/VoteScript/VoteScript.tsx
+++ modules/votes/ui/VoteScript/VoteScript.tsx
@@ -10,16 +10,23 @@ type Props = {
 export function VoteScript({ script }: Props) {
   const { initialLoading, binary, decoded } = useDecodedScript(script)
 
+  console.log(decoded)
+
   return (
     <div>
       <Text color="text" size="xs">
         Action Items
       </Text>
-      {initialLoading ? (
-        <ScriptBox value={binary} />
+      {initialLoading || !decoded?.calls.length ? (
+        <>
+          <Text color="text" size="xxs">
+            Binary script
+          </Text>
+          <ScriptBox value={binary} />
+        </>
       ) : (
         <>
-          {decoded?.calls.map((call, i) => (
+          {decoded.calls.map((call, i) => (
             <Call
               key={call.address + call.methodId + call.encodedCallData}
               id={i + 1}
