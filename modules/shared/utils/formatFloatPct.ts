f447e48eaf64c1d64a732fdcc1096dd24f67caa5
rezed <rezed93@gmail.com>
2022-08-10 19:33:20 +0400

    fix: format percents

--- modules/shared/utils/formatFloatPct.ts
+++ modules/shared/utils/formatFloatPct.ts
@@ -2,10 +2,10 @@ type Args = {
   floor?: boolean
 }
 
-export function formatFloatPct(pct: number, { floor }: Args) {
+export function formatFloatPct(pct: number, args: Args = {}) {
   if (pct < 0.01) {
     return Math.ceil(pct * 10000) / 100
-  } else if (pct > 0.99 || floor) {
+  } else if (pct > 0.99 || args.floor) {
     return Math.floor(pct * 10000) / 100
   }
   return Math.round(pct * 10000) / 100
