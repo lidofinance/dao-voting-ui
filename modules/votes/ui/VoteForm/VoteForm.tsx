4feed43dc5a0767f137a61f6811309ae8346cfb9
rezed <rezed93@gmail.com>
2022-12-19 18:55:34 +0700

    chore: remove unused import

--- modules/votes/ui/VoteForm/VoteForm.tsx
+++ modules/votes/ui/VoteForm/VoteForm.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import { Fragment } from 'react'
 import { useFormVoteInfo } from './useFormVoteInfo'
 import { useFormVoteSubmit } from './useFormVoteSubmit'
 import { useVotePassedCallback } from '../../hooks/useVotePassedCallback'
