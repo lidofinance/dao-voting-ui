b8736080f16ea47b0bea35af7f35cb3279d624cd
rezed <rezed93@gmail.com>
2022-12-21 21:37:34 +0700

    feat: no-blocking wrong network banner

--- modules/blockChain/ui/NetworkSwitcher/NetworkSwitcher.tsx
+++ modules/blockChain/ui/NetworkSwitcher/NetworkSwitcher.tsx
@@ -1,24 +1,25 @@
 import { useConfig } from 'modules/config/hooks/useConfig'
 
 import { Text } from 'modules/shared/ui/Common/Text'
-import { Title } from 'modules/shared/ui/Common/Title'
+import { Container } from '@lidofinance/lido-ui'
+import { Wrap } from './NetworkSwitcherStyle'
+import DangerIconSVG from 'assets/danger.com.svg.react'
 
 import { getChainName } from 'modules/blockChain/chains'
 
 export function NetworkSwitcher() {
   const { supportedChainIds } = useConfig()
+  const networksList = supportedChainIds.map(chainId => getChainName(chainId))
 
   return (
-    <>
-      <Title
-        title="Network does not match"
-        subtitle={<>Please, switch to one of supported network:</>}
-      />
-      <Text size={16} weight={500} isCentered>
-        {supportedChainIds.map(supportedChainId => (
-          <div key={supportedChainId}>{getChainName(supportedChainId)}</div>
-        ))}
-      </Text>
-    </>
+    <Container size="full">
+      <Wrap>
+        <DangerIconSVG />
+        <Text size={12} weight={500}>
+          Unsupported chain. You will not be able to make a vote. Please switch
+          to {networksList.join(', ')} in your wallet.
+        </Text>
+      </Wrap>
+    </Container>
   )
 }
--- modules/blockChain/ui/NetworkSwitcher/NetworkSwitcherStyle.ts
+++ modules/blockChain/ui/NetworkSwitcher/NetworkSwitcherStyle.ts
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
 
-export const NetworksBox = styled.div`
+export const Wrap = styled.div`
   display: flex;
-  justify-content: center;
-  gap: 10px;
+  align-items: center;
+  margin-bottom: ${({ theme }) => theme.spaceMap.xxl}px;
+  padding: ${({ theme }) => theme.spaceMap.lg}px;
+  background-color: ${({ theme }) =>
+    theme.name === 'light' ? '#fffae0' : theme.colors.warning};
+  border-radius: ${({ theme }) => theme.borderRadiusesMap.md}px;
+
+  & svg {
+    display: block;
+    flex: 0 0 auto;
+    margin-right: ${({ theme }) => theme.spaceMap.sm}px;
+    fill: ${({ theme }) => theme.colors.accentText};
+  }
 `
