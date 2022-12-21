447c23e2727a1df0b42e52d06aee3f916b7d828d
rezed <rezed93@gmail.com>
2022-12-21 15:53:21 +0700

    feat: add nprogress to visualize page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,11 +20,15 @@ import { ModalProvider } from 'modules/modal/ModalProvider'
 import { NetworkSwitcher } from 'modules/blockChain/ui/NetworkSwitcher'
 import { VotePromptProvider } from 'modules/votes/providers/VotePrompt'
 
+import { nprogress } from 'modules/shared/utils/nprogress'
 import { parseEnvConfig } from 'modules/config/utils/parseEnvConfig'
 import { getAddressList } from 'modules/config/utils/getAddressList'
 import { withCsp } from 'modules/shared/utils/csp'
 import { CustomAppProps } from 'modules/shared/utils/utilTypes'
 import { CHAINS } from '@lido-sdk/constants'
+
+// Visualize route changes
+nprogress()
 
 const basePath = getConfig().publicRuntimeConfig.basePath || ''
 
