336842e29dc81ac7e902d1e5a40c2a0ffc3860f6
Nikita Kozlov <nikita.kozlov@p2p.org>
2023-02-10 16:41:26 +0300

    feat: updated ts to ^4.9.4 & fixed issues

--- modules/shared/ui/Controls/Form/Form.tsx
+++ modules/shared/ui/Controls/Form/Form.tsx
@@ -1,17 +1,18 @@
 import {
   FormProvider,
   UseFormReturn,
+  FieldValues,
   UseFormHandleSubmit,
 } from 'react-hook-form'
 
-type Props<TFieldValues> = {
+type Props<TFieldValues extends FieldValues> = {
   formMethods: UseFormReturn<TFieldValues>
   onSubmit: Parameters<UseFormHandleSubmit<TFieldValues>>[0]
   className?: string
   children?: React.ReactNode
 }
 
-export function Form<TFieldValues>({
+export function Form<TFieldValues extends FieldValues>({
   formMethods,
   onSubmit,
   className,
