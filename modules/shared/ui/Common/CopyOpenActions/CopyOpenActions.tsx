21a264993a313ac3afe6bdf51cd7a096d9cc657d
Andrei <t.fane@mail.ru>
2023-04-06 15:53:43 +0700

    fix: fix etherscan link

--- modules/shared/ui/Common/CopyOpenActions/CopyOpenActions.tsx
+++ modules/shared/ui/Common/CopyOpenActions/CopyOpenActions.tsx
@@ -1,11 +1,12 @@
-import { useEtherscanOpen } from '@lido-sdk/react'
+import { useCallback } from 'react'
+import { getEtherscanLink, openWindow } from '@lido-sdk/helpers'
+import type { EtherscanEntities } from '@lido-sdk/helpers'
 import { useCopyToClipboard } from 'modules/shared/hooks/useCopyToClipboard'
+import { useWeb3 } from 'modules/blockChain/hooks/useWeb3'
 
 import { ButtonIcon, Copy } from '@lidofinance/lido-ui'
 import { Wrap } from './CopyOpenActionsStyle'
 import { ButtonExternalView } from 'modules/shared/ui/Common/ButtonExternalView'
-
-import type { EtherscanEntities } from '@lido-sdk/helpers'
 
 type Props = {
   value: string | null | undefined
@@ -13,8 +14,13 @@ type Props = {
 }
 
 export function CopyOpenActions({ value, entity }: Props) {
+  const { chainId } = useWeb3()
   const handleCopy = useCopyToClipboard(value ?? '')
-  const handleEtherscan = useEtherscanOpen(value ?? '', entity)
+
+  const handleEtherscan = useCallback(() => {
+    const link = getEtherscanLink(chainId, value ?? '', entity)
+    openWindow(link)
+  }, [chainId, entity, value])
 
   const copyText =
     entity === 'address' ? 'address' : entity === 'tx' ? 'hash' : 'token'
