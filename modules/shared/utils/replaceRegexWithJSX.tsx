8eaa93e9bbc66c27a272a47c0862fd5baecca5db
Dmitrii Podlesnyi <rezed93@gmail.com>
2023-06-21 16:54:40 +0400

    feat: votes metadata links parsed

--- modules/shared/utils/replaceRegexWithJSX.tsx
+++ modules/shared/utils/replaceRegexWithJSX.tsx
@@ -1,13 +1,31 @@
 import { Fragment } from 'react'
 
-export function replaceRegexWithJSX(
-  text: string,
-  regex: RegExp,
-  replace: (substr: string) => JSX.Element,
-) {
-  const textArray = text.split(regex)
-  return textArray.map((str, i) => {
-    const children = regex.test(str) ? replace(str) : str
+export const REGEX_REPLACER_HINT = /(__\$\d+__)/g
+export const REGEX_REPLACER_INDEX = /__\$(\d+)__/g
+
+type ReplaceRule = {
+  regex: RegExp
+  replace: (substr: string) => JSX.Element
+}
+
+export function replaceRegexWithJSX(text: string, rules: ReplaceRule[]) {
+  let textBuffer = text
+  const jsxBuffer: JSX.Element[] = []
+
+  rules.forEach(rule => {
+    textBuffer = textBuffer.replaceAll(rule.regex, match => {
+      jsxBuffer.push(rule.replace(match))
+      return `__$${jsxBuffer.length - 1}__`
+    })
+  })
+
+  const textArray = textBuffer.split(REGEX_REPLACER_HINT)
+
+  const result = textArray.map((str, i) => {
+    const test = REGEX_REPLACER_INDEX.exec(str)
+    const children = test ? jsxBuffer[Number(test[1])] : str
     return <Fragment key={i}>{children}</Fragment>
   })
+
+  return result
 }
