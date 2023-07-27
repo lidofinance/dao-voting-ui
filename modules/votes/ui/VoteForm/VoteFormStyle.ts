84e79bb6c63defe01305c6e099af7b5bb804acf9
Dmitrii Podlesnyi <rezed93@gmail.com>
2023-07-27 18:17:03 +0400

    fix: more cases for rpc error message displaying

--- modules/votes/ui/VoteForm/VoteFormStyle.ts
+++ modules/votes/ui/VoteForm/VoteFormStyle.ts
@@ -10,17 +10,6 @@ export const Desc = styled.div`
   }
 `
 
-export const ErrorMessage = styled.div`
-  padding: ${({ theme }) => theme.spaceMap.lg}px;
-  color: #fff;
-  background-color: var(--lido-color-error);
-  border-radius: ${({ theme }) => theme.borderRadiusesMap.xl}px;
-
-  & a {
-    color: #fff;
-  }
-`
-
 export const ClearButton = styled(Text).attrs({
   size: 'xs',
   weight: 700,
