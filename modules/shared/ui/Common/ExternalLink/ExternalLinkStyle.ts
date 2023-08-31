ddf4409781bb71ebe39b014342b63a3d3bd343d3
Evgenii <dev-www@ya.ru>
2023-08-31 21:30:42 +0400

    fix: change link display type to looks better

--- modules/shared/ui/Common/ExternalLink/ExternalLinkStyle.ts
+++ modules/shared/ui/Common/ExternalLink/ExternalLinkStyle.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const ExternalLinkWrap = styled.span`
-  display: inline-block;
+  display: inline;
   height: ${({ theme }) => theme.spaceMap.lg}px;
   width: fit-content;
   color: var(--lido-color-primary);
