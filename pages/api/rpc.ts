24737de7d18236fb0899e613f0667c6f50397fb2
Evgenii <dev-www@ya.ru>
2024-04-18 22:16:56 +0800

    fix: rollback debug

--- pages/api/rpc.ts
+++ pages/api/rpc.ts
@@ -8,19 +8,19 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const { serverRuntimeConfig } = getConfig()
 const { rpcUrls_1, rpcUrls_5, rpcUrls_17000 } = serverRuntimeConfig
 
-// interface IRpcRequest {
-//   method: string
-//   params: any[]
-//   id: number
-//   jsonrpc: '2.0'
-// }
+interface IRpcRequest {
+  method: string
+  params: any[]
+  id: number
+  jsonrpc: '2.0'
+}
 interface IRpcResponse {
   jsonrpc: '2.0'
   id: number
   result: any[]
 }
 const isLogsRequest = (item: any) => item?.method === 'eth_getLogs'
-// const hasResult = (item: any) => item.data?.result?.length > 0
+const hasResult = (item: any) => item.data?.result?.length > 0
 
 export default async function rpc(req: NextApiRequest, res: NextApiResponse) {
   const RPC_URLS: Record<number, string[]> = {
@@ -66,54 +66,51 @@ export default async function rpc(req: NextApiRequest, res: NextApiResponse) {
 
     // this part for detecting issue with loose answer to eth_getLogs request
     if (requestedFb) {
-      const respondedFb: string = await requestedFb.text()
-      res.status(500).send({ error: respondedFb })
+      const respondedFb: IRpcResponse[] = await requestedFb.json()
+      const mixed = req.body.filter(isLogsRequest).map((item: IRpcRequest) => ({
+        req: item,
+        res: [
+          {
+            data: responded.find(resp => resp.id === item.id),
+            date: requested.headers.get('date'),
+            trace: requested.headers.get('x-drpc-trace-id'),
+          },
+          {
+            data: respondedFb.find(resp => resp.id === item.id),
+            date: requestedFb.headers.get('date'),
+            trace: requestedFb.headers.get('x-drpc-trace-id'),
+          },
+        ],
+      }))
 
-      // const respondedFb: IRpcResponse[] = await requestedFb.json()
-      // const mixed = req.body.filter(isLogsRequest).map((item: IRpcRequest) => ({
-      //   req: item,
-      //   res: [
-      //     {
-      //       data: responded.find(resp => resp.id === item.id),
-      //       date: requested.headers.get('date'),
-      //       trace: requested.headers.get('x-drpc-trace-id'),
-      //     },
-      //     {
-      //       data: respondedFb.find(resp => resp.id === item.id),
-      //       date: requestedFb.headers.get('date'),
-      //       trace: requestedFb.headers.get('x-drpc-trace-id'),
-      //     },
-      //   ],
-      // }))
-      //
-      // mixed.forEach((item: any) => {
-      //   let index = -1
-      //   let message = 'no blockers'
-      //   index = !hasResult(item.res[0]) && hasResult(item.res[1]) ? 0 : index
-      //   index = hasResult(item.res[0]) && !hasResult(item.res[1]) ? 1 : index
-      //
-      //   if (index != -1) {
-      //     message = 'One chain loose rpc logs'
-      //   }
-      //
-      //   if (!hasResult(item.res[0]) && !hasResult(item.res[1])) {
-      //     index = 1
-      //     message = 'Both chains loose rpc logs'
-      //   }
-      //
-      //   if (index != -1) {
-      //     const { data, date, trace } = item.res[index]
-      //     console.info({
-      //       message,
-      //       urlIndex: index,
-      //       chainId,
-      //       date,
-      //       trace,
-      //       req: item.req,
-      //       res: data,
-      //     })
-      //   }
-      // })
+      mixed.forEach((item: any) => {
+        let index = -1
+        let message = 'no blockers'
+        index = !hasResult(item.res[0]) && hasResult(item.res[1]) ? 0 : index
+        index = hasResult(item.res[0]) && !hasResult(item.res[1]) ? 1 : index
+
+        if (index != -1) {
+          message = 'One chain loose rpc logs'
+        }
+
+        if (!hasResult(item.res[0]) && !hasResult(item.res[1])) {
+          index = 1
+          message = 'Both chains loose rpc logs'
+        }
+
+        if (index != -1) {
+          const { data, date, trace } = item.res[index]
+          console.info({
+            message,
+            urlIndex: index,
+            chainId,
+            date,
+            trace,
+            req: item.req,
+            res: data,
+          })
+        }
+      })
     }
 
     res.status(requested.status).json(responded)
