05b8613364cf14b8601ef3265796301afc1cb472
Evgenii <dev-www@ya.ru>
2024-04-18 22:37:30 +0800

    fix: hide error text

--- pages/api/rpc.ts
+++ pages/api/rpc.ts
@@ -124,6 +124,6 @@ export default async function rpc(req: NextApiRequest, res: NextApiResponse) {
       error instanceof Error ? error.message : 'Something went wrong',
       error,
     )
-    res.status(500).send({ error })
+    res.status(500).send({ error: 'Something went wrong' })
   }
 }
