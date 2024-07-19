1d313cf2e93fe2271a211752e88199da784c478e
Alexander Belokon <ikatamarinaki@gmail.com>
2024-07-19 13:10:28 +0200

    refactor: add more return data to useGovernanceBalance

--- modules/tokens/hooks/useGovernanceBalance.ts
+++ modules/tokens/hooks/useGovernanceBalance.ts
@@ -1,10 +1,34 @@
 import { useWeb3 } from 'modules/blockChain/hooks/useWeb3'
 import { ContractGovernanceToken } from 'modules/blockChain/contracts'
+import { useLidoSWR } from '@lido-sdk/react'
+import { CHAINS } from '@lido-sdk/constants'
+import { MiniMeTokenAbi } from 'generated'
+import { formatToken } from '../utils/formatToken'
 
 export function useGovernanceBalance() {
-  const { walletAddress } = useWeb3()
-  return ContractGovernanceToken.useSwrWeb3(
-    walletAddress ? 'balanceOf' : null,
-    [String(walletAddress)],
+  const { walletAddress, chainId } = useWeb3()
+  const governanceToken = ContractGovernanceToken.useRpc()
+
+  return useLidoSWR(
+    walletAddress
+      ? ['swr:useGovernanceBalance', chainId, governanceToken, walletAddress]
+      : null,
+    async (
+      _key: string,
+      _chainId: CHAINS,
+      governanceTokenContract: MiniMeTokenAbi,
+      _walletAddress: string,
+    ) => {
+      const governanceBalance = await governanceTokenContract.balanceOf(
+        _walletAddress,
+      )
+      const governanceSymbol = await governanceTokenContract.symbol()
+      const governanceBalanceStr = formatToken(
+        governanceBalance,
+        governanceSymbol,
+      )
+
+      return { governanceBalance, governanceSymbol, governanceBalanceStr }
+    },
   )
 }
