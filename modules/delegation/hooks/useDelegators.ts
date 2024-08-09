5a3dea7e5cdad85af9099cba07087f8444c94023
Evgenii <dev-www@ya.ru>
2024-08-09 13:50:46 +0400

    test: make async queries, change constants for QA tests

--- modules/delegation/hooks/useDelegators.ts
+++ modules/delegation/hooks/useDelegators.ts
@@ -33,53 +33,42 @@ export function useDelegators() {
 
       const fetchLimit = Math.min(delegatorsCount, DELEGATORS_FETCH_TOTAL)
       const fetchCount = Math.ceil(fetchLimit / DELEGATORS_FETCH_SIZE)
-      const fetchNumbers = Array(fetchCount)
-        .fill(0)
-        .map((_, i) => i)
+      const fetchNumbers = Array(fetchCount).fill(0)
 
       const delegators: { address: string; balance: BigNumber }[] = []
       let fetchedValue = BigNumber.from(0)
-      console.log(fetchCount)
 
-      for (const fetchNumber of fetchNumbers) {
-        const delegatorsAtPage = await voting.getDelegatedVoters(
-          _walletAddress,
-          fetchNumber * DELEGATORS_FETCH_SIZE,
-          DELEGATORS_FETCH_SIZE,
-        )
+      await Promise.all(
+        fetchNumbers.map(async (_, fetchIndex) => {
+          const delegatorsAtPage = await voting.getDelegatedVoters(
+            _walletAddress,
+            fetchIndex * DELEGATORS_FETCH_SIZE,
+            DELEGATORS_FETCH_SIZE,
+          )
 
-        if (delegatorsAtPage.length === 0) {
-          continue
-        }
+          if (delegatorsAtPage.length === 0) {
+            return
+          }
 
-        const delegatorsAtPageBalances = await voting.getVotingPowerMultiple(
-          delegatorsAtPage,
-        )
+          const delegatorsAtPageBalances = await voting.getVotingPowerMultiple(
+            delegatorsAtPage,
+          )
 
-        delegatorsAtPage.forEach((delegator, index) => {
-          delegators.push({
-            address: delegator,
-            balance: delegatorsAtPageBalances[index],
+          delegatorsAtPage.forEach((delegator, index) => {
+            delegators.push({
+              address: delegator,
+              balance: delegatorsAtPageBalances[index],
+            })
+            fetchedValue = fetchedValue.add(delegatorsAtPageBalances[index])
           })
-          console.log(delegatorsAtPageBalances[index].toString())
-          fetchedValue = fetchedValue.add(delegatorsAtPageBalances[index])
-        })
-      }
-      console.log(delegators)
+        }),
+      )
+
       const fetchedCount = delegators.length
-      console.log(delegators.length)
 
       const wealthyDelegators = delegators.filter(delegator =>
         delegator.balance.gt(VP_MIN_TO_SHOW),
       )
-      console.log(wealthyDelegators)
-      console.log({
-        totalCount: delegatorsCount,
-        fetchedCount,
-        wealthyCount: wealthyDelegators.length,
-        list: wealthyDelegators,
-        fetchedValue,
-      })
       return {
         totalCount: delegatorsCount,
         fetchedCount,
