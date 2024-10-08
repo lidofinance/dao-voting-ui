675a2f20c7b7e56013ca3a581c72f898341afed4
Alexander Belokon <ikatamarinaki@gmail.com>
2024-10-08 13:48:10 +0200

    fix: replace delegate images with local links

--- modules/delegation/publicDelegates.ts
+++ modules/delegation/publicDelegates.ts
@@ -1,113 +1,103 @@
+import getConfig from 'next/config'
+
+const basePath = getConfig().publicRuntimeConfig.basePath || ''
+
 // The list of public delegates was provided by DAO Ops workstream member
 export const PUBLIC_DELEGATES = [
   {
     name: 'Matt Stam',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/mattstam/288/2888_2.png',
+    avatar: `${basePath}/delegates/matt.jpeg`,
     address: '0x33379367200Ac200182ccD4abD71683F2D24e373',
     lido: 'https://research.lido.fi/t/matt-stam-delegate-thread/8145',
     twitter: 'https://x.com/mattstam_eth',
   },
   {
     name: 'Ignas',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/ignas/288/4798_2.png',
+    avatar: `${basePath}/delegates/ignas.png`,
     address: '0x3DDC7d25c7a1dc381443e491Bbf1Caa8928A05B0',
     lido: 'https://research.lido.fi/t/ignas-delegate-thread/8135',
     twitter: 'https://x.com/DefiIgnas',
   },
   {
     name: 'Wintermute Governance',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/wintermute/288/3963_2.png',
+    avatar: `${basePath}/delegates/wintermute.png`,
     address: '0xB933AEe47C438f22DE0747D57fc239FE37878Dd1',
     lido: 'https://research.lido.fi/t/wintermute-governance-delegate-thread/8131',
     twitter: 'https://x.com/wintermute_t',
   },
   {
     name: 'WPRC (WhitePaper Reading Club)',
-    avatar:
-      'https://images.squarespace-cdn.com/content/v1/65be10b23014452b13c31611/af6739ef-4a80-4175-8d04-eb7f7a5e260f/IMG_2592.png?format=1500w',
+    avatar: `${basePath}/delegates/wprc.png`,
     address: '0x8Ab6612BbcF7E133A6BB03b3264718d30f25e0BA',
     lido: 'https://research.lido.fi/t/whitepaper-reading-club-delegate-thread/8100',
     twitter: 'https://x.com/wpreadingclub',
   },
   {
     name: 'Tane',
-    avatar:
-      'https://europe1.discourse-cdn.com/business20/uploads/lido/original/2X/c/c24f99b7e88e3edac9cb5a09a5cbf1ee55c54b1c.png',
+    avatar: `${basePath}/delegates/tane.png`,
     address: '0xB79294D00848a3A4C00c22D9367F19B4280689D7',
     lido: 'https://research.lido.fi/t/tane-delegate-thread/7639',
     twitter: 'https://x.com/tanelabs',
   },
   {
     name: 'SEEDOrg',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/seedorg/288/4048_2.png',
+    avatar: `${basePath}/delegates/seedorg.jpeg`,
     address: '0xc1c2e8a21b86e41d1e706c232a2db5581b3524f8',
     lido: 'https://research.lido.fi/t/seedgov-delegate-thread/7643',
     twitter: 'https://x.com/SEEDGov',
   },
   {
     name: 'Anthony Leuts',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/leuts/288/4427_2.png',
+    avatar: `${basePath}/delegates/leuts.jpeg`,
     address: '0x42E6DD8D517abB3E4f6611Ca53a8D1243C183fB0',
     lido: 'https://research.lido.fi/t/anthony-leuts-delegate-thread/8019',
     twitter: 'https://x.com/A_Leutenegger',
   },
   {
     name: 'Sov',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/sov/288/4684_2.png',
+    avatar: `${basePath}/delegates/sov.jpeg`,
     address: 'sovereignsignal.eth',
     lido: 'https://research.lido.fi/t/sov-delegate-thread/8069',
     twitter: 'https://x.com/sovereignsignal',
   },
   {
     name: 'Polar',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/polar/288/4806_2.png',
+    avatar: `${basePath}/delegates/polar.jpeg`,
     address: '0x1f76a6Bf03429480472B3695E08689219cE15ED6',
     lido: 'https://research.lido.fi/t/polar-delegate-thread/8048',
     twitter: 'https://x.com/post_polar_',
   },
   {
     name: 'TokenLogic',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/tokenlogic/288/2945_2.png',
+    avatar: `${basePath}/delegates/tokenlogic.png`,
     address: '0x2cc1ADE245020FC5AAE66Ad443e1F66e01c54Df1',
     lido: 'https://research.lido.fi/t/tokenlogic-delegate-thread/8066',
     twitter: 'https://x.com/Token_Logic',
   },
   {
     name: 'BlockworksResearch',
-    avatar:
-      'https://europe1.discourse-cdn.com/business20/uploads/lido/original/2X/e/eb38115f63cd4faf2565e776b3aa110564d76132.jpeg',
+    avatar: `${basePath}/delegates/tokenlogic.png`,
     address: '0xfF4139e99Bd7c23F4611dc660c33c97A825EA67b',
     lido: 'https://research.lido.fi/t/blockworks-research-delegate-thread/8024',
     twitter: 'https://x.com/blockworksres',
   },
   {
     name: 'Blockful (Daniela Zschaber)',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/danimim/288/4785_2.png',
+    avatar: `${basePath}/delegates/blockful.jpeg`,
     address: '0x3b9F47629cD4D5903cF3eB897aaC4F6b41Dd2589',
     lido: 'https://research.lido.fi/t/daniela-zschaber-representing-blockful-delegate-thread/8018',
     twitter: 'https://x.com/danimimm',
   },
   {
     name: 'karpatkey',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/karpatkey/288/2307_2.png',
+    avatar: `${basePath}/delegates/karpatkey.png`,
     address: '0x8787FC2De4De95c53e5E3a4e5459247D9773ea52',
     lido: 'https://research.lido.fi/t/karpatkey-delegate-thread/8011',
     twitter: 'https://x.com/karpatkey',
   },
   {
     name: 'StableLab',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/stable_lab/288/3053_2.png',
+    avatar: `${basePath}/delegates/stablelab.png`,
     address: 'stablelab.eth',
     lido: 'https://research.lido.fi/t/stablelab-delegate-thread-updated/4904/17',
     twitter: 'https://x.com/Stablelab',
@@ -121,80 +111,70 @@ export const PUBLIC_DELEGATES = [
   },
   {
     name: 'NodeSoda',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/fible1/288/4854_2.png',
+    avatar: `${basePath}/delegates/nodesoda.jpeg`,
     address: '0x000b4369B71B6634F27F5De9Cbaaabb0D21B8be5',
     lido: 'https://research.lido.fi/t/nodesoda-com-delegate-thread/8031',
     twitter: 'https://x.com/pablolema85',
   },
   {
     name: 'eboadom (Ernesto)',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/eboadom/288/3764_2.png',
+    avatar: `${basePath}/delegates/eboadom.png`,
     address: '0x5Ef980c7bdA50c81E8FB13DfF2b804113065ED1c',
     lido: 'https://research.lido.fi/t/eboadom-delegate-thread/8079',
     twitter: 'https://x.com/eboadom',
   },
   {
     name: 'ReservoirDAO',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/sharp/288/4749_2.png',
+    avatar: `${basePath}/delegates/reservoir.jpeg`,
     address: '0x4f894Bfc9481110278C356adE1473eBe2127Fd3C',
     lido: 'https://research.lido.fi/t/reservoirdao-alphagrowth-delegate-platform/8169',
     twitter: 'https://x.com/alphagrowth1',
   },
   {
     name: 'FranklinDAO',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/franklindao/288/4879_2.png',
+    avatar: `${basePath}/delegates/franklin.jpeg`,
     address: '0x070341aA5Ed571f0FB2c4a5641409B1A46b4961b',
     lido: 'https://research.lido.fi/t/franklin-dao-delegate-platform/8167',
     twitter: 'https://x.com/franklin_dao',
   },
   {
     name: 'Mog',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/mogglet/288/4834_2.png',
+    avatar: `${basePath}/delegates/mog.jpeg`,
     address: '0x61Cc8f06F3B6e1a3Fba28CbEbc4c89304b1187D1',
     lido: 'https://research.lido.fi/t/mog-delegate-thread/8164',
     twitter: 'https://x.com/tungtdinh',
   },
   {
     name: 'Pol Lanski',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/lanski/288/4846_2.png',
+    avatar: `${basePath}/delegates/lanski.png`,
     address: 'lanski.eth',
     lido: 'https://research.lido.fi/t/pol-lanski-delegate-thread/8155',
     twitter: 'https://x.com/Pol_Lanski',
   },
   {
     name: 'notjamiedimon',
-    avatar:
-      'https://pbs.twimg.com/profile_images/1821464105515163648/ddtnBp5f_400x400.jpg',
+    avatar: `${basePath}/delegates/notjamiedimon.jpg`,
     address: 'notjamiedimon.eth',
     lido: 'https://research.lido.fi/t/notjamiedimon-delegate-thread/8174',
     twitter: 'https://x.com/regentrading',
   },
   {
     name: 'Irina',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/irina_everstake/288/2107_2.png',
+    avatar: `${basePath}/delegates/irina.jpeg`,
     address: '0x06A90756e57bC7A016Eed0aB23fC36d11C42bBa0',
     lido: 'https://research.lido.fi/t/irina-delegate-thread/8217',
     twitter: 'https://x.com/eth_everstake',
   },
   {
     name: 'marcbcs',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/marcbcs/288/2659_2.png',
+    avatar: `${basePath}/delegates/marcbcs.jpeg`,
     address: '0x98308b6dA79B47D15e9438CB66831563649Dbd94',
     lido: 'https://research.lido.fi/t/marcbcs-delegate-thread/8209',
     twitter: 'https://x.com/marcbcs',
   },
   {
     name: 'Daedalus Collective',
-    avatar:
-      'https://europe1.discourse-cdn.com/business20/uploads/lido/original/2X/3/34aed82e52916083ae6b83fc17510409371527a6.png',
+    avatar: `${basePath}/delegates/daedalus.png`,
     address: '0x04827a54F2e345467beAFEfB9EF76Cb2f2c62D83',
     lido: 'https://research.lido.fi/t/daedalus-delegate-thread/8195',
     twitter: 'https://x.com/daedalus_angels',
@@ -215,24 +195,21 @@ export const PUBLIC_DELEGATES = [
   },
   {
     name: 'Next Finance Tech',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/nextfinancetech/288/4870_2.png',
+    avatar: `${basePath}/delegates/nxt.jpeg`,
     address: '0x18c674F655594F15c490aeEac737895b7903E37f',
     lido: 'https://research.lido.fi/t/next-finance-tech-delegate-thread/8204',
     twitter: 'https://x.com/nxt_fintech',
   },
   {
     name: 'cp0x',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/cp0x/288/4886_2.png',
+    avatar: `${basePath}/delegates/cp0x.jpeg`,
     address: '0x6f9BB7e454f5B3eb2310343f0E99269dC2BB8A1d',
     lido: 'https://research.lido.fi/t/cp0x-delegate-thread/8193',
     twitter: 'https://x.com/cp0xdotcom',
   },
   {
     name: 'Simply Staking',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/damien/288/4853_2.png',
+    avatar: `${basePath}/delegates/simplystaking.png`,
     address: '0xCeDF324843775c9E7f695251001531798545614B',
     lido: 'https://research.lido.fi/t/simply-staking-delegate-thread/8178',
     twitter: 'https://x.com/SimplyStaking',
@@ -253,32 +230,28 @@ export const PUBLIC_DELEGATES = [
   },
   {
     name: 'Lemma Solutions',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/lemma/288/4873_2.png',
+    avatar: `${basePath}/delegates/lemma.jpeg`,
     address: '0x58B1b454Dbe5156ACc8FC2139E7238451b59f432',
     lido: 'https://research.lido.fi/t/lemma-delegate-thread/8214',
     twitter: 'https://x.com/Lemma_Solutions',
   },
   {
     name: 'Nansen',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/nansen/288/4925_2.png',
+    avatar: `${basePath}/delegates/nansen.png`,
     address: 'nansengov.eth',
     lido: 'https://research.lido.fi/t/nansen-delegate-thread/8303',
     twitter: 'https://x.com/nansen_ai',
   },
   {
     name: 'DAOplomats',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/daoplomats.eth/288/4877_2.png',
+    avatar: `${basePath}/delegates/daoplomats.png`,
     address: '0x057928bc52bD08e4D7cE24bF47E01cE99E074048',
     lido: 'https://research.lido.fi/t/daoplomats-delegate-thread/8242',
     twitter: 'https://x.com/DAOplomats',
   },
   {
     name: 'PGov.eth',
-    avatar:
-      'https://dub1.discourse-cdn.com/business20/user_avatar/research.lido.fi/pgov/288/4894_2.png',
+    avatar: `${basePath}/delegates/pgov.jpeg`,
     address: 'pgov.eth',
     lido: 'https://research.lido.fi/t/pgov-delegate-thread/8232',
     twitter: 'https://x.com/PGovTeam',
