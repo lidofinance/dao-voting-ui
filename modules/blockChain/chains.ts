4c1b1271ea7aafadd7ea900dcff30e31ac86d487
Alexander Belokon <ikatamarinaki@gmail.com>
2024-10-24 17:53:35 +0200

    refactor: remove Goerli mentions

--- modules/blockChain/chains.ts
+++ modules/blockChain/chains.ts
@@ -3,15 +3,6 @@ import { CHAINS } from '@lido-sdk/constants'
 
 export const ChainNames = {
   [CHAINS.Mainnet]: 'Mainnet',
-  [CHAINS.Ropsten]: 'Ropsten',
-  [CHAINS.Rinkeby]: 'Rinkeby',
-  [CHAINS.Goerli]: 'Goerli',
-  [CHAINS.Kovan]: 'Kovan',
-  [CHAINS.Kintsugi]: 'Kintsugi',
-  [CHAINS.Kiln]: 'Kiln',
-  [CHAINS.Moonbeam]: 'Moonbeam',
-  [CHAINS.Moonriver]: 'Moonriver',
-  [CHAINS.Moonbase]: 'Moonbase',
   [CHAINS.Holesky]: 'Holesky',
 } as const
 
--- modules/blockChain/utils/getGnosisSafeLink.ts
+++ modules/blockChain/utils/getGnosisSafeLink.ts
@@ -1,20 +1,9 @@
-import get from 'lodash/get'
 import { CHAINS } from '@lido-sdk/constants'
-
-const PREFIXES = {
-  [CHAINS.Mainnet]: 'eth',
-  [CHAINS.Goerli]: 'gor',
-  [CHAINS.Holesky]: 'holesky',
-} as const
 
 export const getGnosisSafeLink = (chainId: CHAINS, address: string) => {
   if (chainId === CHAINS.Holesky) {
     return `https://holesky-safe.protofire.io/transactions/queue?safe=holesky:${address}`
   }
 
-  return `https://app.safe.global/transactions/queue?safe=${get(
-    PREFIXES,
-    chainId,
-    '?',
-  )}:${address}`
+  return `https://app.safe.global/transactions/queue?safe=eth:${address}`
 }
