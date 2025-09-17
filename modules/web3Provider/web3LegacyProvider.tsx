1d1eaa53a94a805c55e2cc1a7cde2d0bc6b02bf4
Alexander Belokon <ikatamarinaki@gmail.com>
2025-09-17 21:36:24 +0200

    fix: set web3provider network as any

--- modules/web3Provider/web3LegacyProvider.tsx
+++ modules/web3Provider/web3LegacyProvider.tsx
@@ -11,13 +11,9 @@ import type { Account, Client, Transport } from 'viem'
 
 // Official wagmi adapter for ethers v5 (from wagmi docs)
 export function clientToSigner(client: Client<Transport, Chain, Account>) {
-  const { account, chain, transport } = client
-  const network = {
-    chainId: chain.id,
-    name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
-  }
-  const provider = new providers.Web3Provider(transport, network)
+  const { account, transport } = client
+
+  const provider = new providers.Web3Provider(transport, 'any')
   const signer = provider.getSigner(account.address)
   return signer
 }
